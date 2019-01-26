2278c338c1593d4b661d3ffa88f5a6260403c115
Anish George <anishgeo@thoughtworks.com>
2019-01-26 11:36:33 +0530

    Initial commit

---
+++ test/src/com/anishgeorge/tddsale/SetupTest.java
@@ -0,0 +1,12 @@
+package com.anishgeorge.tddsale;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SetupTest {
+
+    @Test
+    public void isThisWorking() {
+        Assert.assertTrue(true);
+    }
+}

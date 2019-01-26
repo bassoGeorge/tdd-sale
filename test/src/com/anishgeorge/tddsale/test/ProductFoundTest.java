cb234f4c0e4d5112c18303ddfd20ad57633e743b
Anish George <anishgeo@thoughtworks.com>
2019-01-26 11:47:26 +0530

    Move output closer to input, display is part of sale now

--- test/src/com/anishgeorge/tddsale/test/ProductFoundTest.java
+++ test/src/com/anishgeorge/tddsale/test/ProductFoundTest.java
@@ -1,10 +1,7 @@
 package com.anishgeorge.tddsale.test;
 
+import org.junit.Ignore;
 import org.junit.Test;
-
-import javax.swing.*;
-
-import java.awt.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,21 +9,46 @@ public class ProductFoundTest {
 
     @Test
     public void oneProductFound() {
-        Sale sale = new Sale();
         Display display = new Display();
+        Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
         assertEquals("$7.99", display.getText());
     }
 
+    @Test
+    @Ignore("Refactoring")
+    public void secondProductFound() {
+        Display display = new Display();
+        Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+        assertEquals("$8.32", display.getText());
+    }
+
     public static class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+
         public void onBarcode(String barcode) {
+            display.setText("$7.99");
         }
     }
 
     public static class Display {
+
+        private String text;
+
         public String getText() {
-            return "$7.99";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }

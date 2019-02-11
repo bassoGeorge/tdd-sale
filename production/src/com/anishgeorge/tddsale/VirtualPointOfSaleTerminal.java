3e72241dcde76050510256b2d943b7ebe6b9eb4a
Anish George <anishgeo@thoughtworks.com>
2019-02-11 23:19:03 +0530

    It Works (tm)

--- production/src/com/anishgeorge/tddsale/VirtualPointOfSaleTerminal.java
+++ production/src/com/anishgeorge/tddsale/VirtualPointOfSaleTerminal.java
@@ -1,9 +1,20 @@
 package com.anishgeorge.tddsale;
 
-import java.util.Collections;
+import java.util.HashMap;
 
 public class VirtualPointOfSaleTerminal {
     public static void main(String[] args) {
-        new SaleController(new InMemoryCatalog(Collections.emptyMap()), new EnglishLanguageConsoleDisplay());
+        InMemoryCatalog catalog = new InMemoryCatalog(new HashMap<String, Price>() {{
+            put("12345", Price.cents(795));
+        }});
+
+        EnglishLanguageConsoleDisplay display = new EnglishLanguageConsoleDisplay();
+
+        SaleController saleController = new SaleController(catalog, display);
+
+        saleController.onBarcode("12345");
+        saleController.onBarcode("23456");
+        saleController.onBarcode("");
+
     }
 }
